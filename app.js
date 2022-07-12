99600fa0002f801d0d3ddf42ac06c19963461ea6
Ashu Sharma <ashusharma0771@gmail.com>
2022-07-12 16:58:21 +0530

    complete initial Database integration

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Database integration
+
+const URL = "mongodb://localhost:27017/wikiDB";
+mongoose.connect(URL);
+
+// Article Schema
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+const myarticle = new Article({
+  title: "How to win friends?",
+  content:
+    "How to win friends is a really popular book on human communication and relations.",
+});
+
+myarticle.save();
+
 // Handle get request on root route
 
 app.get("/", (req, res) => {
@@ -23,5 +45,7 @@ const port = 3000;
 app.listen(port, (err) => {
   if (!err) {
     console.log(`Sever started on port ${port}`);
+  } else {
+    console.log("Reporting erro while running app on  port ");
   }
 });
