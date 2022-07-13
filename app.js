8485438fca091c177bcb5b9980c4676a76c4e72d
Ashu Sharma <ashusharma0771@gmail.com>
2022-07-13 14:40:52 +0530

    api completed

--- app.js
+++ app.js
@@ -29,48 +29,102 @@ const Article = mongoose.model("Article", articleSchema);
 
 // Handle get request on /articles route
 
-app.get("/articles", (req, res) => {
-  Article.find({}, (err, foundArticles) => {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      console.log("Error occured while finding articles");
-      errorReporter.reportError(err);
-      res.send(err);
-    }
+app
+  .route("/articles")
+  .get((req, res) => {
+    Article.find({}, (err, foundArticles) => {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        console.log("Error occured while finding articles");
+        errorReporter.reportError(err);
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save((err) => {
+      if (!err) {
+        res.send("Successfully fully added a new article");
+      } else {
+        errorReporter.reportError(err);
+        res.send(err);
+      }
+    });
+    console.log(newArticle);
+  })
+  .delete((req, res) => {
+    Article.deleteMany({}, (err) => {
+      if (!err) {
+        res.send("Successfully deleted all articles");
+      } else {
+        res.send(err);
+        errorReporter.reportError(err);
+      }
+    });
   });
-});
 
-// Handle post request on /articles
+// Handle  request on Particular Article.
 
-app.post("/articles", (req, res) => {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
+app
+  .route("/articles/:articleTitle")
+  .get((req, res) => {
+    const articleTitle = req.params.articleTitle;
+    Article.findOne({ title: articleTitle }, (err, foundArticle) => {
+      if (!err) {
+        if (foundArticle) {
+          res.send(foundArticle);
+        } else {
+          res.send("No Article with matching title found");
+        }
+      } else {
+        res.send(err);
+        errorReporter.reportError(err);
+      }
+    });
+  })
+  .put((req, res) => {
+    const articleTitle = req.params.articleTitle;
+    Article.updateOne(
+      { title: articleTitle },
+      { title: req.body.title, content: req.body.content },
+      (err) => {
+        if (!err) {
+          res.send("Successfully updated article.");
+        }
+      }
+    );
+  })
+  .patch(async (req, res) => {
+    await Article.updateOne(
+      {
+        title: req.params.articleTitle, //conditions
+      },
+      {
+        $set: req.body, //updates
+      },
+      (err) => {
+        if (!err) {
+          res.send("OK");
+        } else {
+          res.send(err);
+        }
+      }
+    ).clone();
+  })
+  .delete((req, res) => {
+    Article.deleteOne({ title: req.params.articleTitle }, (err) => {
+      if (!err) {
+        res.send("Successfully deleted the correct article");
+      } else {
+        res.send(err);
+      }
+    });
   });
-  newArticle.save((err) => {
-    if (!err) {
-      res.send("Successfully fully added a new article");
-    } else {
-      errorReporter.reportError(err);
-      res.send(err);
-    }
-  });
-  console.log(newArticle);
-});
-
-// Handle delete request
-
-app.delete("/articles", (req, res) => {
-  Article.deleteMany({}, (err) => {
-    if (!err) {
-      res.send("Successfully deleted all articles");
-    } else {
-      res.send(err);
-      errorReporter.reportError(err);
-    }
-  });
-});
 
 // Listening for HTTP request on specfic port
 const port = 3000;
